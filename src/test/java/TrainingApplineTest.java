0f47a8cc2e9a8e2f9f2b32b84f640cd99546be47
Evgeny <88150928+uneverov@users.noreply.github.com>
2023-06-14 04:06:21 +1000

    Delete .idea directory

--- src/test/java/TrainingApplineTest.java
+++ src/test/java/TrainingApplineTest.java
@@ -3,6 +3,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -14,21 +15,32 @@ public class TrainingApplineTest {
     WebDriver driver;
     WebDriverWait wait;
     String baseUrl = "http://training.appline.ru/user/login";
+    String login = "Секретарь";
+    String password = "testing";
     @Before
     public void before(){
         System.setProperty("webdriver.chrome.driver", "webdriver/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
 
-        wait = new WebDriverWait(driver, 10, 2000);
+        wait = new WebDriverWait(driver, 20, 2000);
         driver.get(baseUrl);
     }
     @Test
     public void trainingApplineTest(){
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id=\"prependedInput\"]")));
-
+        WebElement loginField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id=\"prependedInput\"]")));
+        WebElement passwordField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id=\"prependedInput2\"]")));
+        loginField.sendKeys(login);
+        passwordField.sendKeys(password);
+        WebElement submitButton = driver.findElement(By.xpath("//*[@name=\"_submit\"]"));
+        submitButton.click();
+        try{
+            Thread.sleep(5000);
+        }
+        catch(InterruptedException ignored){
+        }
     }
     @After
     public void after(){
