5e239a00faabbb7cf2ebbf846b1302e280761abc
uneverov <uneverov@gmail.com>
2023-06-14 03:59:25 +1000

    Initial commit

---
+++ src/test/java/TrainingApplineTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class TrainingApplineTest {
+    @Test
+    public void trainingApplineTest(){
+    }
+}
